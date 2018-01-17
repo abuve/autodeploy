fc84eabbe9d25cebf90964e97c38614d9cf11ec4
abuve <656774238@qq.com>
2018-01-17 11:44:26 +0800

    update settings file

--- conf/settings.py
+++ conf/settings.py
@@ -1,2 +1,8 @@
 project_port = 8001
 web_conf_path = "./config_data"
+
+# SSH Configuration
+ssh_key_file = 'D:\MyDocument\私钥文件\id_rsa_aaron'
+ssh_username = 'snadmin'
+ssh_key_pass = '191411'
+ssh_port = 2556
