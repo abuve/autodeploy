82f3462d93e0411be001a8af50e1b6ce1904a6d4
abuve <656774238@qq.com>
2018-01-22 13:31:14 +0800

    update sso

--- repository/urls.py
+++ repository/urls.py
@@ -3,23 +3,25 @@ from repository.views import web_config
 from repository.views import group_config
 from repository.views import instance_config
 
+from django.contrib.auth.decorators import login_required
+
 urlpatterns = [
 
-    url(r'^webconf/nginx/config-(?P<server_id>\d+).html$', web_config.WebConfigNginxView.as_view()),
-    url(r'^webconf/nginx/fileTreeJson-(?P<server_id>\d+).html$', web_config.WebConfigNginxJsonView.as_view()),
-    url(r'^webconf/nginx/version-json-(?P<server_id>\d+).html$', web_config.WebConfigVersionJsonView.as_view()),
-    url(r'^webconf/nginx/version-tree-(?P<server_id>\d+).html$', web_config.WebConfigVersionListView.as_view()),
-    url(r'^webconf/nginx/file-data-(?P<server_id>\d+).html$', web_config.WebConfigFileData.as_view()),
-    url(r'^webconf/nginx/file-push-(?P<server_id>\d+).html$', web_config.WebConfigFilePush.as_view()),
+    url(r'^webconf/nginx/config-(?P<server_id>\d+).html$', login_required(web_config.WebConfigNginxView.as_view())),
+    url(r'^webconf/nginx/fileTreeJson-(?P<server_id>\d+).html$', login_required(web_config.WebConfigNginxJsonView.as_view())),
+    url(r'^webconf/nginx/version-json-(?P<server_id>\d+).html$', login_required(web_config.WebConfigVersionJsonView.as_view())),
+    url(r'^webconf/nginx/version-tree-(?P<server_id>\d+).html$', login_required(web_config.WebConfigVersionListView.as_view())),
+    url(r'^webconf/nginx/file-data-(?P<server_id>\d+).html$', login_required(web_config.WebConfigFileData.as_view())),
+    url(r'^webconf/nginx/file-push-(?P<server_id>\d+).html$', login_required(web_config.WebConfigFilePush.as_view())),
 
-    url(r'^webconf/nginx/get_version_status.html$', web_config.WebConfigVersionStatus.as_view()),
+    url(r'^webconf/nginx/get_version_status.html$', login_required(web_config.WebConfigVersionStatus.as_view())),
 
-    url(r'^config/group/(?P<server_id>\d+).html$', group_config.GroupConfigView.as_view(), name='test123'),
-    url(r'^config/group/json-(?P<server_id>\d+).html$', group_config.GroupConfigJsonView.as_view()),
-    url(r'^config/group/update-server-group.html$', group_config.UpdateServerGroupView.as_view()),
+    url(r'^config/group/(?P<server_id>\d+).html$', login_required(group_config.GroupConfigView.as_view())),
+    url(r'^config/group/json-(?P<server_id>\d+).html$', login_required(group_config.GroupConfigJsonView.as_view())),
+    url(r'^config/group/update-server-group.html$', login_required(group_config.UpdateServerGroupView.as_view())),
 
-    url(r'^config/instance/(?P<server_id>\d+).html$', instance_config.InstanceConfigView.as_view()),
-    url(r'^config/instance/json-(?P<server_id>\d+).html$', instance_config.InstanceConfigJsonView.as_view()),
-    url(r'^config/instance/update-server-instance.html$', instance_config.UpdateServerInstanceView.as_view()),
+    url(r'^config/instance/(?P<server_id>\d+).html$', login_required(instance_config.InstanceConfigView.as_view())),
+    url(r'^config/instance/json-(?P<server_id>\d+).html$', login_required(instance_config.InstanceConfigJsonView.as_view())),
+    url(r'^config/instance/update-server-instance.html$', login_required(instance_config.UpdateServerInstanceView.as_view())),
 
 ]
--- web/urls.py
+++ web/urls.py
@@ -9,6 +9,8 @@ from web.views import server
 from web.views import mission
 from web.views import project
 
+from django.contrib.auth.decorators import login_required
+
 urlpatterns = [
     # url(r'^login.html$', account.LoginView.as_view()),
     # url(r'^logout.html$', account.LogoutView.as_view()),
@@ -28,13 +30,13 @@ urlpatterns = [
     # url(r'^user.html$', user.UserJsonView.as_view()),
 
 
-    url(r'^server.html$', server.ServerListView.as_view()),
-    url(r'^servers.html$', server.ServerJsonView.as_view()),
-    url(r'^server-config-(?P<asset_nid>\d+).html$', server.ServerDetailView.as_view(), name='server-config'),
-    url(r'^add-server.html$', server.AddServerView.as_view()),
-    url(r'^edit-server-(?P<server_nid>\d+).html$', server.UpdateServerView.as_view()),
+    url(r'^server.html$', login_required(server.ServerListView.as_view())),
+    url(r'^servers.html$', login_required(server.ServerJsonView.as_view())),
+    url(r'^server-config-(?P<asset_nid>\d+).html$', login_required(server.ServerDetailView.as_view()), name='server-config'),
+    url(r'^add-server.html$', login_required(server.AddServerView.as_view())),
+    url(r'^edit-server-(?P<server_nid>\d+).html$', login_required(server.UpdateServerView.as_view())),
 
-    url(r'^get_app_by_project/$', server.get_app_by_project),
+    url(r'^get_app_by_project/$', login_required(server.get_app_by_project)),
 
     # url(r'^update-server-group.html$', server.UpdateServerGroupView.as_view()),
     # url(r'^server-groups-(?P<asset_nid>\d+).html$', server.ServerDetaiGroupView.as_view()),
@@ -45,19 +47,19 @@ urlpatterns = [
 
     #url(r'^update-yaml-nginx.html$', server.UpdateYamlConfView.as_view()),
 
-    url(r'^mission.html$', mission.MissionListView.as_view()),
-    url(r'^missions.html$', mission.MissionJsonView.as_view()),
-    url(r'^mission-create.html$', mission.MissionCreateView.as_view()),
-    url(r'^mission-detail-(?P<mission_id>\d+).html$', mission.MissionDetailListView.as_view()),
-    url(r'^mission-detail-json-(?P<mission_id>\d+).html$', mission.MissionDetailListJsonView.as_view()),
+    url(r'^mission.html$', login_required(mission.MissionListView.as_view())),
+    url(r'^missions.html$', login_required(mission.MissionJsonView.as_view())),
+    url(r'^mission-create.html$', login_required(mission.MissionCreateView.as_view())),
+    url(r'^mission-detail-(?P<mission_id>\d+).html$', login_required(mission.MissionDetailListView.as_view())),
+    url(r'^mission-detail-json-(?P<mission_id>\d+).html$', login_required(mission.MissionDetailListJsonView.as_view())),
 
-    url(r'^mission-detail-json-(?P<mission_id>\d+).html$', mission.MissionDetailListJsonView.as_view()),
+    url(r'^mission-detail-json-(?P<mission_id>\d+).html$', login_required(mission.MissionDetailListJsonView.as_view())),
 
 
-    url(r'^project.html$', project.ProjectListView.as_view()),
-    url(r'^projects.html$', project.ProjectJsonView.as_view()),
-    url(r'^add-project.html$', project.AddProjectView.as_view()),
-    url(r'^edit-project-(?P<project_nid>\d+).html$', project.UpdateProjectView.as_view()),
+    url(r'^project.html$', login_required(project.ProjectListView.as_view())),
+    url(r'^projects.html$', login_required(project.ProjectJsonView.as_view())),
+    url(r'^add-project.html$', login_required(project.AddProjectView.as_view())),
+    url(r'^edit-project-(?P<project_nid>\d+).html$', login_required(project.UpdateProjectView.as_view())),
 
     # url(r'^chart-(?P<chart_type>\w+).html$', home.ChartView.as_view()),
 ]
