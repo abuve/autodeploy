1e65791ec639ae91a02459de0b64fbe4c55aaf3c
abuve <656774238@qq.com>
2018-02-07 17:31:02 +0800

    update cmdb asset function

--- cmdb/urls.py
+++ cmdb/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     url(r'^business-detail-(?P<business_nid>\d+).html$', login_required(business.BusinessDetailView.as_view())),
 
     # cmdb report with no asset id, it's need admin to approved.
-    url(r'^report/asset_report_use_asset_id/$', approval.asset_report_use_asset_id),
-    url(r'^report/asset_report_with_no_id/$', approval.asset_with_no_asset_id),
+    # url(r'^report/asset_report_use_asset_id/$', approval.asset_report_use_asset_id),
+    # url(r'^report/asset_report_with_no_id/$', approval.asset_with_no_asset_id),
 
     url(r'^idc-list.html$', login_required(idc.IdcListView.as_view())),
     url(r'^idcs.html$', login_required(idc.IdcJsonView.as_view())),
