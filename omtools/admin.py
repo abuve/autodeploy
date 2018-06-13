014c78f98b33cf0e4ba5473e51420e5b5c8b69ae
abuve <656774238@qq.com>
2018-06-13 11:43:00 +0800

    update cmdb server logs

--- omtools/admin.py
+++ omtools/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from omtools import models
 
+class AccessLogsControl(admin.ModelAdmin):
+    list_display = ('project_id', 'server_node', 'server_type', 'logs_type', 'url', 'memo')
+    search_fields = ('server_node', 'server_type', 'logs_type', 'url', 'memo')
+
+
 admin.site.register(models.MongodbMission)
 admin.site.register(models.MongodbMissionTemplate)
-admin.site.register(models.LogsControl)
+admin.site.register(models.LogsControl, AccessLogsControl)
